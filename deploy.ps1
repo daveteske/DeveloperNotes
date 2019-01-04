bc5f069c09e0d5a92a41fab9326bc2439d8138be
Dave Teske <daveteske@gmail.com>
2019-01-04 11:52:08 -0500

    still fiddling around

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+
+
+# StartProcess("git", "checkout master");
+# StartProcess("git", "add .");
+# StartProcess("git", "commit -m \"Checking output in for subtree\"");
+
+git subtree split --prefix docs/output -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
+
+# StartProcess("git", "subtree split --prefix docs/output -b gh-pages");
+# StartProcess("git", "push -f origin gh-pages:gh-pages");
+# StartProcess("git", "branch -D gh-pages");
